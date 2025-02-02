0402ba8b7a26b8599dbbbc864cb07f827126433b
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-02-01 18:34:39 -0800

    trying to figure it out if the problem that I have here is for everything or just in my device. Also, Dynamic Parameters added.

--- books.py
+++ books.py
@@ -12,6 +12,11 @@ BOOKS = [
     {"title": "IT", "author": "Stephen King", "category":"Horror"},
 ]
 
+
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+@app.get("/books/{dynamic_param}")
+async def read_all_books(dynamic_param):
+    return{'dynamic_param': dynamic_param}
