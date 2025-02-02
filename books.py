21b5a4c81482385cc7337a95d3df923c411ab48d
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-02-01 17:51:09 -0800

    Authors' name appears, now I'll introduce Swagger

--- books.py
+++ books.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+
+BOOKS = [
+    {"title": "Dune", "author": "Frank Herbert", "category":"science fiction"},
+    {"title": "Divine Comedy", "author": "Dante Aligheri", "category":"Fiction"},
+    {"title": "Harry Potter", "author": "J.K Rowling", "category":"Fantasy"},
+    {"title": "Lord of the Rings", "author": "J.R.R Tolkien", "category":"Fantasy"},
+    {"title": "The Alchemist", "author": "Paulo Coelho", "category":"Philosophy"},
+    {"title": "IT", "author": "Stephen King", "category":"Horror"},
+]
+
+@app.get("/api-endpoint")
 async def first_api():
-    return {"message": "Hello!"}
+    return BOOKS
