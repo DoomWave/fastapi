1d02c9592e29036d3f4b984c9456c5ef4102e00c
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-02-01 17:54:58 -0800

    def changed to read_all_books

--- books.py
+++ books.py
@@ -12,6 +12,6 @@ BOOKS = [
     {"title": "IT", "author": "Stephen King", "category":"Horror"},
 ]
 
-@app.get("/api-endpoint")
-async def first_api():
+@app.get("/books")
+async def read_all_books():
     return BOOKS
