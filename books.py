253bca0b9afdf66a85beaaa7348170f42abd8b6e
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-02-01 19:24:49 -0800

    I finished learning about path parameters.

--- books.py
+++ books.py
@@ -13,10 +13,16 @@ BOOKS = [
 ]
 
 
-@app.get("/books")
+@app.get("/Books")
 async def read_all_books():
     return BOOKS
 
-@app.get("/books/{dynamic_param}")
-async def read_all_books(dynamic_param):
-    return{'dynamic_param': dynamic_param}
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get("title").casefold() == book_title.casefold():
+            return book
+
+
+
